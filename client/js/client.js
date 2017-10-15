858c5cc7f9279783e1c4be1736e2f481f21257cc
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-10-15 01:07:11 -0400

    added some chat features

--- client/js/client.js
+++ client/js/client.js
@@ -1,11 +1,30 @@
 var sock = io();
-sock.on('msg',onMessage);
+sock.on('msg', onMessage);
+
 function onMessage(text) {
-	var list = document.getElementById('chat');
-	var el = document.createElement('li');
+	var list = document.getElementById('chat-space');
+	var el = document.createElement('p');
 	el.innerHTML = text;
 	list.appendChild(el);
 }
 
 // onMessage('Hi');
 // onMessage('I am connected');
+
+var form = document.getElementById('chat-form');
+form.addEventListener('submit', function(e) {
+    var input = document.getElementById('chat-input');
+    var value = input.value;
+    input.value = '';
+    sock.emit('msg', value);
+    e.preventDefault();
+});
+
+function addTurnListener(id) {
+    var button = document.getElementById(id);
+    button.addEventListener('click', function() {
+        sock.emit('turn', id);
+    });
+}
+
+// ['rock', 'paper', 'scissors'].forEach(addTurnListener);
--- server.js
+++ server.js
@@ -3,13 +3,34 @@
 let http = require('http');
 let express = require('express');
 let socketio = require('socket.io');
+// let Game = require('./Game');
 
 let app = express();
 let server = http.createServer(app);
 let io = socketio(server);
 
+let waitingPlayer;
+
 // sock --> sending between client/server
-io.on('connection',(sock) => sock.emit('msg','Hello!'));
+io.on('connection', onConnection);
 
 app.use(express.static(__dirname+'/client'));
 server.listen(8080,() => console.log('Ready to work'));
+
+function onConnection(sock) {
+    sock.emit('msg', 'Hello! Get ready for a round of GIF My Thing!');
+
+    // whenever the client sends a message, send txt to all clients.
+    // in this case, whenever it gets a msg, it sends msg back to
+    // all of the clients connected to the server
+    sock.on('msg', (txt) => io.emit('msg', txt));
+
+    if (waitingPlayer) {
+        // new Game(waitingPlayer, sock);
+        waitingPlayer = null;
+    }
+    else {
+        waitingPlayer = sock;
+        sock.emit('msg', 'You are waiting for a second player');
+    }
+}
